36a61642359b08dc062e101cc4b4f00f8894fb0c
khyatigupta183 <guptadiya206@gmail.com>
2026-09-10 19:04:47 +0530

    code + questions

--- BinarySearch/code/src/com/khyati/SearchInMountain.java
+++ BinarySearch/code/src/com/khyati/SearchInMountain.java
@@ -2,14 +2,20 @@ package com.khyati;
 
 public class SearchInMountain {
     static void main(String[] args) {
-        
+
     }
 // https://leetcode.com/problems/find-in-mountain-array/description/
 
-    int search(int[] arr, int target){
-
+    int search(int[] arr, int target) {
+        int peak = peakIndexInMountainArray(arr);
+        int firstTry = orderagnosticBS(arr, target, 0, peak);
+        if (firstTry != -1) {
+            return firstTry;
+        }
+        // try to search in second half
+    return orderagnosticBS(arr, target, peak+1,arr.length-1);
     }
-    static int peakIndexInMountainArray(int[]arr){
+   public static int peakIndexInMountainArray(int[]arr){
 
     int start = 0;
     int end = arr.length -1;
@@ -33,21 +39,42 @@ public class SearchInMountain {
     // and if we are saying that one item is remaining, hence cuz of above line that is the best possible ans
         return start; // or return end as best ans
 }
-    static int binarySearch(int[] arr , int target, int start,int end){
+    static int orderagnosticBS(int[] arr, int target, int start,int end ){
+
+
+        //find whether the array is sorted in ascending or descending
+        boolean isAsc =  arr[start] < arr[end];
+//        if(arr[start] < arr[end]){
+//            isAsc = true;
+//        } else {
+//            isAsc = false;
+//        }
+
         while (start <= end){
             //find the middle element
 //            int mid = (start+end)/2; // might be possible that (start +end)exceed the range of int in java
-            int mid = start + (end-start)/2;
+            int mid = start + (end -start)/2;
 
-            if(target < arr[mid]){
-                end = mid - 1;
-            }else if (target > arr[mid]){
-                start = mid + 1;
-            }else{
-                //ans found
+            if(arr[mid] == target){
                 return mid;
             }
+
+            if(isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                }
+            }else {
+                if(target > arr[mid]){
+                    end = mid - 1;
+                }else if (target < arr[mid]){
+                    start = mid + 1;
+                }
+            }
+
         }
+
         return -1;
     }
 }
