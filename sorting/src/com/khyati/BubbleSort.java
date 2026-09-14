c6d3b68dded883d544913c69697c756569e8d9dd
khyatigupta183 <guptadiya206@gmail.com>
2026-09-14 13:47:18 +0530

    bubble sort

---
+++ sorting/src/com/khyati/BubbleSort.java
@@ -0,0 +1,36 @@
+package com.khyati;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    static void main(String[] args) {
+     int[] arr =  {1, 2, 3, 4, 5};
+     bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        boolean swapped ;
+        // run the steps n-1 times
+        for(int i =0;  i < arr.length;  i++){
+//            int swapcount =0;
+            swapped= false;
+            // for each step, max item will come at the last respective index
+            for(int j =1; j < arr.length-i; j++){
+                // swap if the item is samaller than the previous item
+                if(arr[j] < arr[j-1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j]= arr[j-1];
+                    arr[j-1]= temp;
+//                    swapcount++;
+                    swapped = true;
+                }
+            }
+            // if ypu did not swap for a particular value of i; it means the array is sorted hence stop the program
+//            if (swapCount == 0) break;  // stop early
+            if(!swapped){ // !false=true
+                break;
+            }
+        }
+    }
+}
