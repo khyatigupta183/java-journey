9d802716e3165897ac19c05a4df60a8024fdd75e
khyatigupta183 <guptadiya206@gmail.com>
2026-09-14 17:37:56 +0530

    bubble + selection sort

--- sorting/src/com/khyati/Main.java
+++ sorting/src/com/khyati/Main.java
@@ -2,12 +2,41 @@ package com.khyati;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class Main {
     static void main(String[] args) {
      int[] arr =  {1, 2, 3, 4, 5};
-     bubble(arr);
+     selection(arr);
+//     bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+
+        }
+    }
+
+    static void swap (int[] arr, int first, int second){
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+
+    }
+
+        private static int getMaxIndex(int[] arr, int start, int end){
+            int max = start;
+
+            for(int i =start; i<=end; i++){
+                if(arr[max]< arr[i]){
+                    max = i ;
+                }
+            }
+            return max;
+        }
+
     static void bubble(int[] arr){
         boolean swapped ;
         // run the steps n-1 times
@@ -34,3 +63,4 @@ public class BubbleSort {
         }
     }
 }
+
