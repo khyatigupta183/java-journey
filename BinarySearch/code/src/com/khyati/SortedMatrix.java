9823d1b99909d463ada27ab0ab48803b544cbe49
khyatigupta183 <guptadiya206@gmail.com>
2026-09-14 13:47:53 +0530

    binary search

--- BinarySearch/code/src/com/khyati/SortedMatrix.java
+++ BinarySearch/code/src/com/khyati/SortedMatrix.java
@@ -6,7 +6,15 @@ public class SortedMatrix {
     }
      static int[] binarySearch(int[] matrix, int row, int cStart, int cEnd, int target){
         while(cStart <= cEnd){
-            
+            int mid = cStart+(cEnd-cStart)/2;
+            if(matrix[row][mid]== target){
+                return new int[]{row, mid};
+            }
+            if( matrix[row][mid]< target){
+
+            }else{
+
+            }
         }
      }
     static int[] search(int[][] matrix, int target){
